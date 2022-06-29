c3f2cdf339bc76b6d47fb5b907812cb6f45489dc
Nicholas Duca <98131371+nduca1@users.noreply.github.com>
2022-06-29 18:06:53 -0400

    Update Signup.js

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -118,7 +118,7 @@ function Signup() {
             required
           />
           <div className={`${errorPassword ? "form-error" : undefined}`}>
-            <span>{errorPassword && "Please enter a valid email"} </span>
+            <span>{errorPassword && "Password must be 8 characters long and must contain 1 Uppercase letter, 1 Lowercase letter, and 1 special character "} </span>
           </div>
         </div>
 
